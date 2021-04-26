bcded599c0e75da71a7befbcdc4768867b9765f9
lcv004 <luiggicuevas92@gmail.com>
2021-04-25 22:14:49 -0400

    Add Inventory class and update UML Diagram.

---
+++ Source/Inventory.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace restaurant
+{
+    // This class represents the stock
+    // of products the restaurant has.
+    class Inventory
+    {
+        // Attributes
+        public int ID { get;, set; }
+        public List<Product> Products { get;, set; };
+        public List<int> ProductQuantities { get;, set; };
+    }
+}
