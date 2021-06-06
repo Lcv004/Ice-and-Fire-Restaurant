17fbe5befca6fb0225cb4d655c1aac46fffce110
lcv004 <luiggicuevas92@gmail.com>
2021-06-05 23:30:37 -0400

    Implements inventory repository

---
+++ Source/IFR.Services/Repositories/InventoryRepository.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using IFR.Entity;
+
+namespace IFR.Services.Repositories
+{
+    public class InventoryRepository : IRepository<long, Inventory>
+    {
+        Dictionary<long, Inventory> _inventoryDictionary;
+        long _id;
+
+        private InventoryRepository()
+        {
+             _inventoryDictionary = new Dictionary<long, Inventory>();
+             _id = 0;
+        }
+
+        public void Add(Inventory entity)
+        {
+            _id ++;
+            _inventoryDictionary.Add(_id, entity);
+        }
+
+        public void Remove(long key)
+        {
+            if (_inventoryDictionary.ContainsKey(key))
+            {
+                _inventoryDictionary.Remove(key);
+            }
+        }
+
+        public Inventory Get(long key)
+        {
+            return _inventoryDictionary[key];
+        }
+
+        public IEnumerable<Inventory> GetAll()
+        {
+            return _inventoryDictionary.Values;
+        }
+    }
+}
